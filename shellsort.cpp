dced0d77ceb9be60cf027cc11ccd6c3dee6ddf8e
Denay2468 <denayaung13@gmail.com>
2026-04-09 20:45:32 +0630

    add some code to put elements

--- shellsort.cpp
+++ shellsort.cpp
@@ -14,5 +14,14 @@ void input() {
     else
        cout << "\nArray can have maximum 20 elements.\n";
    }
+
+    cout << "\n-------------------\n";
+    cout << "Enter array elements\n";
+    cout << "-------------------\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Element " << (i + 1) << ": ";
+        cin >> arr[i];
+    }
    
 }
