e2f7f32169a06e5e9252cfd00c2f5d0ed85162b3
Denay2468 <denayaung13@gmail.com>
2026-04-09 20:49:28 +0630

    create shellsort function and code inside

--- shellsort.cpp
+++ shellsort.cpp
@@ -25,3 +25,29 @@ void input() {
     }
    
 }
+
+void shellsort(){
+    int temp,j;
+    int pass = 0;
+    for (int gap = n/2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            temp = arr[i];
+            j = i;
+            while (j >= gap && arr[j - gap] > temp)
+            {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+            cout << "\nAfter pass " << pass + 1 << ": ";
+            for (int k = 0; k < n; k++)
+            {
+                cout << arr[k] << " ";
+            }
+
+        pass++;
+    }
+}
